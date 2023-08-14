f45a207a1697a5071412a1516bd4b531e73c047e
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2023-08-14 22:20:17 +0200

    Add first simple test that tries to checkout on worktree branch
    This test is not working properly. In the test mode, IDEA creates
    a test file manager that is not working
    with `OpenEditorsContextProvider`.

--- src/test/kotlin/git4idea/test/GitPlatformTest.kt
+++ src/test/kotlin/git4idea/test/GitPlatformTest.kt
@@ -1,10 +1,5 @@
 package git4idea.test
 
-import com.intellij.vcs.test.VcsPlatformTest
 import com.intellij.testFramework.HeavyPlatformTestCase
 
-abstract class GitPlatformTest : VcsPlatformTest() {
-    fun test() {
-
-    }
-}
+abstract class GitPlatformTest : HeavyPlatformTestCase()

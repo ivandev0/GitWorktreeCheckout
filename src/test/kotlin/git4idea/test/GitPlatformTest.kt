6e695113f0c6dd537c7560f7f782760ae41cc8de
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-02-16 13:12:22 +0100

    Add test to check the path fix process for multiple files

--- src/test/kotlin/git4idea/test/GitPlatformTest.kt
+++ src/test/kotlin/git4idea/test/GitPlatformTest.kt
@@ -1,7 +1,10 @@
 package git4idea.test
 
+import com.intellij.ide.DataManager
 import com.intellij.notification.Notification
+import com.intellij.notification.NotificationAction
 import com.intellij.notification.Notifications
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
@@ -10,6 +13,8 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.testFramework.HeavyPlatformTestCase
 import com.intellij.util.io.write
 import com.intellij.util.messages.Topic
+import junit.framework.TestCase
+import org.kylchik.gitworktreecheckout.worktree.GitWorktreePathFixer
 import java.io.File
 import java.io.FileOutputStream
 import java.io.OutputStreamWriter
@@ -18,11 +23,11 @@ import java.nio.file.Path
 import java.nio.file.Paths
 
 abstract class GitPlatformTest : HeavyPlatformTestCase() {
-    protected fun createFile(dir: String, fileName: String, content: String): VirtualFile {
+    protected fun createFile(dir: String, fileName: String, content: String = ""): VirtualFile {
         return createFile(Paths.get(dir), fileName, content)
     }
 
-    protected fun createFile(dir: Path, fileName: String, content: String): VirtualFile {
+    protected fun createFile(dir: Path, fileName: String, content: String = ""): VirtualFile {
         val file = createTempFile(dir, fileName, content)
         val stream = FileOutputStream(file)
         OutputStreamWriter(stream, Charsets.UTF_8).use { writer ->
@@ -42,10 +47,18 @@ abstract class GitPlatformTest : HeavyPlatformTestCase() {
         return file.toFile()
     }
 
+    protected fun FileEditorManager.openFiles(vararg filePath: Path): List<FileEditor> {
+        return filePath.flatMap { openFile(it) }
+    }
+
     protected fun FileEditorManager.openFile(filePath: Path): List<FileEditor> {
         val txtFileInB = VfsUtil.findFileByIoFile(filePath.toFile(), true)!!
         val descriptor = OpenFileDescriptor(project, txtFileInB)
         return this.openFileEditor(descriptor, true)
+    }
+
+    protected fun FileEditorManager.closeAllOpenedFiles() {
+        openFiles.forEach { closeFile(it) }
     }
 
     protected fun subscribeToNotifications(
@@ -58,5 +71,22 @@ abstract class GitPlatformTest : HeavyPlatformTestCase() {
                 container += notification
             }
         })
+    }
+
+    protected fun fixPath() {
+        val notifications = mutableListOf<Notification>()
+        subscribeToNotifications(notifications)
+
+        GitWorktreePathFixer().process(project)
+
+        TestCase.assertEquals(1, notifications.size)
+        notifications.first().callAction()
+    }
+
+    private fun Notification.callAction() {
+        val changePathAction = this.actions.first()
+        // TODO use `TestActionEvent.createTestEvent` instead of `AnActionEvent.createFromAnAction`
+        val event = AnActionEvent.createFromAnAction(changePathAction, null, "", DataManager.getInstance().dataContext)
+        (changePathAction as? NotificationAction)?.actionPerformed(event, this)
     }
 }

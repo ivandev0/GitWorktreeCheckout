d9df95f867190bda6213d84baf3117463d07d237
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-03-03 12:56:31 +0100

    Change the minimal supported version to be "241"
    Earlier plugin used `openFiles` method directly, but
    it was removed starting from "231" version. For now, the
    only feasible way is to use the internal
    `restoreEditors` method.

--- src/main/kotlin/org/kylchik/gitworktreecheckout/worktree/GitWorktreePathFixer.kt
+++ src/main/kotlin/org/kylchik/gitworktreecheckout/worktree/GitWorktreePathFixer.kt
@@ -6,11 +6,17 @@ import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.impl.EditorSplitterState
+import com.intellij.openapi.fileEditor.impl.EditorsSplitters
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl
 import com.intellij.openapi.project.Project
+import com.intellij.platform.ide.progress.runWithModalProgressBlocking
+import com.intellij.platform.util.progress.reportRawProgress
 import com.intellij.tasks.context.WorkingContextProvider
 import git4idea.repo.GitRepositoryManager
 import org.jdom.Element
+import java.awt.KeyboardFocusManager
+import java.awt.Window
 import java.io.File
 import java.io.FileFilter
 
@@ -78,13 +84,28 @@ private class ChangePathAction(
     override fun actionPerformed(e: AnActionEvent, notification: Notification) {
         val project = e.project ?: return
         val fileEditorManager = getProdFileEditorManager(project) ?: return
-        fixPathsInElement(element, projectsPaths)
 
-        fileEditorManager.mainSplitters.removeAll()
-        fileEditorManager.mainSplitters.readExternal(element)
-        fileEditorManager.mainSplitters.openFiles()
+        runWithModalProgressBlocking(project, "Fixing editors after branch switch") {
+            reportRawProgress {
+                fixPathsInElement(element, projectsPaths)
+
+                @Suppress("UnstableApiUsage")
+                fileEditorManager.mainSplitters.restoreEditors(EditorSplitterState(element), editorHadFocus())
+            }
+        }
 
         notification.expire()
+    }
+
+    // Copied from `OpenEditorsContextProvider`
+    private fun editorHadFocus(): Boolean {
+        var c = KeyboardFocusManager.getCurrentKeyboardFocusManager().focusOwner
+        while (c != null) {
+            if (c is Window) return false
+            if (c is EditorsSplitters) return true
+            c = c.parent
+        }
+        return true // if editor was focused, but was closed by `clearContext` call, we'll get here
     }
 }
 
